dacebe00aeba3b72d7e20e33b778d06003fa64da
ablil <ablil@protonmail.com>
2021-04-05 09:22:23 +0100

    feat: allow user to reset password
    Also move login/regiter code from auth page to login/register componenet.

---
+++ src/components/auth/ForgetPassword.tsx
@@ -0,0 +1,50 @@
+import react, { useState } from "react";
+import { auth } from "../../config/firebase";
+
+const ForgetPassword = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  const onForgetPassword = () => {
+    if (email.length) {
+      setIsSubmited(true);
+      auth
+        .sendPasswordResetEmail(email)
+        .then((_) => {})
+        .catch((err) => console.error(err));
+    }
+  };
+
+  return isSubmited ? (
+    <section className="h-full w-full center-with-flex">
+      <section className="auth-wrapper p-8 w-full md:w-2/4">
+        If your email is associated with an account, you will receive a reset
+        link by email
+      </section>
+    </section>
+  ) : (
+    <section className="h-full w-full center-with-flex">
+      <section className="auth-wrapper p-4 w-full md:w-2/4 lg:w-1/4">
+        <header>Did you forgot your password ?</header>
+        <article>
+          <input
+            type="email"
+            className="form-input my-4"
+            placeholder="Type your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            onClick={onForgetPassword}
+            className="btn btn-blue my-4"
+            type="submit"
+          >
+            Send reset link
+          </button>
+        </article>
+      </section>
+    </section>
+  );
+};
+
+export default ForgetPassword;
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
-import { ReactComponent as LoginImage } from "../assets/images/dev-productivity.svg";
-import { ReactComponent as RegisterImage } from "../assets/images/new-task.svg";
 import Register from "../components/auth/Register";
 import Login from "../components/auth/Login";
+import ForgetPassword from "../components/auth/ForgetPassword";
 
 const AuthPage = () => {
   let location = useLocation();
@@ -11,43 +10,13 @@ const AuthPage = () => {
   return (
     <Switch location={location}>
       <Route exact path="/login">
-        <section className="bg-image h-full fade-in flex flex-col lg:flex-row">
-          <article className="lg:w-2/4">
-            <div className="text-white text-center w-3/4 mx-auto py-12">
-              <h1 className="text-5xl">Keep tracks of your ideas</h1>
-              <h3 className="text-md py-4">
-                It is time to start that side project, you bought a domain name
-                for.
-              </h3>
-            </div>
-            <div className="center-with-flex py-4">
-              <RegisterImage className="hidden lg:block w-3/4 h-auto" />
-            </div>
-          </article>
-          <article className="lg:w-2/4 py-12">
-            <Login />
-          </article>
-        </section>
+        <Login />
       </Route>
       <Route exact path="/register">
-        <section className="bg-image fade-in flex flex-col-reverse lg:flex-row">
-          <article className="lg:w-2/4 py-12">
-            <Register />
-          </article>
-          <article className="lg:w-2/4">
-            <div className="text-white text-center w-3/4 mx-auto py-12">
-              <h1 className="text-2xl">
-                You will never know when you get that one million $$ idea
-              </h1>
-              <h3 className="text-md py-4">
-                Create an account and keep tracks of you side projects
-              </h3>
-            </div>
-            <div className="center-with-flex py-4">
-              <LoginImage className="hidden lg:block w-3/4 h-auto" />
-            </div>
-          </article>
-        </section>
+        <Register />
+      </Route>
+      <Route exact path="/reset">
+        <ForgetPassword />
       </Route>
     </Switch>
   );
