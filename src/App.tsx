cf7acaef46cf38582f483965c14d0340e0d9f241
ablil <ablil@protonmail.com>
2021-06-28 13:08:39 +0200

    refactor: add meta tag to html page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./components/commons/404";
 import PrivateRoute from "./components/PrivateRoute";
+import Helmet from "react-helmet";
 
 import "./config/firebase";
 import AuthPage from "./pages/AuthPage";
@@ -11,6 +12,15 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="keep track of your projects ideas" />
+        <meta
+          name="keywords"
+          content="project, idea, dev, tab, web, software, app"
+        />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      </Helmet>
       <Router>
         <Switch>
           <Route exact path="/login" component={AuthPage} />
